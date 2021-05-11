676d0a8eb21f446a654803d95da73e18d2cbcb87
Bharti gupta <bhartigupta1587@gmail.com>
2021-05-11 16:33:15 -0500

    croce error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,9 @@ import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { BooksComponent } from './books/books.component';
-import { AuthorComponent } from './books/author/author.component';
-import { PublisherComponent } from './books/publisher/publisher.component';
-import { GenreComponent } from './books/genre/genre.component';
+import { AuthorComponent } from './author/author.component';
+import { PublisherComponent } from './publisher/publisher.component';
+import { GenreComponent } from './genre/genre.component';
 
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
