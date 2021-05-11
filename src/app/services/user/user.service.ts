aec43265c7a9a4ec696ed5cf5ca7f6fddc01f0a5
Bharti gupta <bhartigupta1587@gmail.com>
2021-05-11 14:48:37 -0500

    conditional nev bar working

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -29,13 +29,17 @@ export class UserService {
         localStorage.setItem('currentUser', `${user.userName}`);
         localStorage.setItem('token', `${token}`);
         console.log(response, token);
+        this.currentUser = user.userName;
+        this.searchSubject.next(this.currentUser);
+        this.router.navigate(['/books']);
       }, err => console.log(err));
   }
 
   logoutUser(): void {
     localStorage.removeItem('currentUser');
     localStorage.removeItem('token');
-    this.currentUser = '';
+    this.currentUser = 'null';
+    this.searchSubject.next(this.currentUser);
     this.router.navigate(['/login']);
   }
 }
