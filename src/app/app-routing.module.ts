2d40d49232251ea35dff0c2d8451a096d98e1b16
Bharti gupta <bhartigupta1587@gmail.com>
2021-05-11 13:52:42 -0500

    logo and nevbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BooksComponent} from './books/books.component';
+import {AuthorComponent} from './books/author/author.component';
+import {PublisherComponent} from './books/publisher/publisher.component';
+import {GenreComponent} from './books/genre/genre.component';
+import {LoginComponent} from './login/login.component';
+import {SignupComponent} from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'books',
+    component: BooksComponent
+  },
+  {
+    path: 'author',
+    component: AuthorComponent
+  },
+  {
+    path: 'publisher',
+    component: PublisherComponent
+  },
+  {
+    path: 'genre',
+    component: GenreComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books.component';
+import { AuthorComponent } from './books/author/author.component';
+import { PublisherComponent } from './books/publisher/publisher.component';
+import { GenreComponent } from './books/genre/genre.component';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    HomeComponent,
+    BooksComponent,
+    AuthorComponent,
+    PublisherComponent,
+    GenreComponent
   ],
   imports: [
     BrowserModule,
